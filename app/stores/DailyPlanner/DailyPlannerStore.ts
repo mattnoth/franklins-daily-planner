8437a5dcb9ee1709e58f9efebd9853b144d93e19
mattnoth <musicnoth@gmail.com>
2023-08-26 10:28:46 +0200

    Add DailyPlannerStore

--- app/stores/DailyPlanner/DailyPlannerStore.ts
+++ app/stores/DailyPlanner/DailyPlannerStore.ts
@@ -1,1 +1,29 @@
-// PLACEHOLDER FILE 
+import { types, Instance } from "mobx-state-tree";
+import { IActivityStore, ActivityStore } from '../Activity/ActivityStore';
+
+const DailyPlannerStore = types
+  .model({
+    dailyActivities: types.map(ActivityStore),
+  })
+  .views(self => ({
+    get totalDays() {
+      return self.dailyActivities.size;
+    }
+  }))
+  .actions(self => ({
+    addDay(date: string) {
+      self.dailyActivities.set(date, ActivityStore.create({ activities: [] }));
+    },
+    removeDay(date: string) {
+      self.dailyActivities.delete(date);
+    },
+    // ... other actions for manipulating the daily activities
+  }));
+
+// Initial instance
+const dailyPlannerStore = DailyPlannerStore.create({ dailyActivities: {} });
+
+export { dailyPlannerStore, DailyPlannerStore };
+
+// If you want to use types elsewhere
+export type IDailyPlannerStore = Instance<typeof DailyPlannerStore>;
